3030ee2182776147f5b4b53a934390104f3b6c92
Hannah Sun <hannah.sun@datadoghq.com>
2020-07-23 15:15:24 -0400

    docstring for redirectHandlers

--- src/redirect.ts
+++ src/redirect.ts
@@ -6,6 +6,12 @@ export const JS_HANDLER_WITH_LAYERS =
   "/opt/nodejs/node_modules/datadog-lambda-js/handler.handler";
 export const JS_HANDLER = "node_modules/datadog-lambda-js/dist/handler.handler";
 
+/**
+ * To avoid modifying code in the user's lambda handler, redirect the handler to a Datadog
+ * handler that initializes the Lambda Layers and then calls the original handler.
+ * 'DD_LAMBDA_HANDLER' is set to the original handler in the lambda's environment for the
+ * replacement handler to find.
+ */
 export function redirectHandlers(
   lambdas: LambdaFunction[],
   addLayers: boolean
