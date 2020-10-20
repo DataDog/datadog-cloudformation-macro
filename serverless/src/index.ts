0ea788a2cb9e63f9c4d7094ba2235a4e6cc2de00
dylan.yang <dylan.yang@datadoghq.com>
2020-10-20 14:53:11 -0400

    autodetect when to use govcloud layer

--- serverless/src/index.ts
+++ serverless/src/index.ts
@@ -65,13 +65,7 @@ export const handler = async (event: InputEvent, _: any) => {
 
     // Apply layers
     if (config.addLayers) {
-      const errors = applyLayers(
-        region,
-        lambdas,
-        config.pythonLayerVersion,
-        config.nodeLayerVersion,
-        config.isGovCloud,
-      );
+      const errors = applyLayers(region, lambdas, config.pythonLayerVersion, config.nodeLayerVersion);
       if (errors.length > 0) {
         return {
           requestId: event.requestId,
