94c386de9e0960d3deb7b4394f350d9c6a2865e7
Stephen Firrincieli <sfirrincieli@gmail.com>
2023-04-19 15:15:04 -0400

    Update docstring in serverless/src/git.ts
    Co-authored-by: jordan gonzález <30836115+duncanista@users.noreply.github.com>

--- serverless/src/git.ts
+++ serverless/src/git.ts
@@ -27,15 +27,15 @@ export const filterAndFormatGithubRemote = (rawRemote: string | undefined): stri
   rawRemote = rawRemote.replace(/git@github\.com:|https:\/\/github\.com\//, "github.com/");
 
   return rawRemote;
-};
-
-export const getGitTagsFromParam = (gitDataParam: string) => {
-  const [commitSha, rawGitConfigUrlOutput] = gitDataParam.split(",", 2);
-
-  const sanitizedRemote = filterAndFormatGithubRemote(rawGitConfigUrlOutput);
-
-  const gitCommitShaTag = `git.commit.sha:${commitSha}`;
-  const gitRepoUrlTag = `git.repository_url:${sanitizedRemote}`;
-
-  return { gitCommitShaTag, gitRepoUrlTag };
-};
+/**
+ * Removes sensitive information from the given git remote
+ * URL and normalizes the URL prefix.
+ * 
+ * @example
+ * // returns "github.com/"
+ * filterAndFormatGithubRemote("git@github.com")
+ * filterAndFormatGithubRemote("https://github.com")
+ * 
+ * @param rawRemote git remote URL.
+ * @returns 
+ */
