29927e1b4e0464a8cec8364f98d4e7ebbaf482a9
Stephen Firincieli <stephen.firrincieli@datadoghq.com>
2023-05-16 16:32:15 -0400

    Update example template

--- serverless/examples/sam-python-datadog-hello-world/hello_world/app.py
+++ serverless/examples/sam-python-datadog-hello-world/hello_world/app.py
@@ -37,7 +37,7 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "Hello Datadog 3!",
+                "message": "Hello Datadog!",
                 # "location": ip.text.replace("\n", "")
             }
         ),
