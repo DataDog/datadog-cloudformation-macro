940dff4341bc8135799cb7d4e5072390081e1abd
Yiming Luo <yiming.luo@datadoghq.com>
2025-01-06 16:33:04 -0500

    [Step Function] 3. Create log group

--- serverless/src/step_function/log.ts
+++ serverless/src/step_function/log.ts
@@ -6,8 +6,9 @@ import { StateMachine } from "./types";
  * Set up logging for the given state machine:
  * 1. Set log level to ALL
  * 2. Set includeExecutionData to true
+ * 3. Create a destination log group (if not set already)
  */
-export function setUpLogging(_resources: Resources, stateMachine: StateMachine): void {
+export function setUpLogging(resources: Resources, stateMachine: StateMachine): void {
   log.debug(`Setting up logging`);
   if (!stateMachine.properties.LoggingConfiguration) {
     stateMachine.properties.LoggingConfiguration = {};
@@ -17,4 +18,42 @@ export function setUpLogging(_resources: Resources, stateMachine: StateMachine):
 
   logConfig.Level = "ALL";
   logConfig.IncludeExecutionData = true;
+
+  if (!logConfig.Destinations) {
+    log.debug(`Log destination not found, creating one`);
+    const logGroupKey = createLogGroup(resources, stateMachine);
+    logConfig.Destinations = [
+      {
+        CloudWatchLogsLogGroup: {
+          LogGroupArn: {
+            "Fn::GetAtt": [logGroupKey, "Arn"],
+          },
+        },
+      },
+    ];
+  } else {
+    log.debug(`Log destination already exists, skipping creating one`);
+  }
 }
+
+function createLogGroup(resources: Resources, stateMachine: StateMachine): string {
+  const logGroupKey = `${stateMachine.resourceKey}LogGroup`;
+  resources[logGroupKey] = {
+    Type: "AWS::Logs::LogGroup",
+    Properties: {
+      LogGroupName: buildLogGroupName(stateMachine, undefined),
+      RetentionInDays: 7,
+    },
+  };
+
+  return logGroupKey;
+}
+
+/**
+ * Builds log group name for a state machine.
+ * @returns log group name like "/aws/vendedlogs/states/MyStateMachine-Logs" (without env)
+ *                           or "/aws/vendedlogs/states/MyStateMachine-Logs-dev" (with env)
+ */
+export const buildLogGroupName = (stateMachine: StateMachine, env: string | undefined): string => {
+  return `/aws/vendedlogs/states/${stateMachine.resourceKey}-Logs${env !== undefined ? "-" + env : ""}`;
+};
