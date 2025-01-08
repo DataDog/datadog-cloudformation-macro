640eb76de4b63c8c7e3ea66e713427b35f61a532
Yiming Luo <yiming.luo@datadoghq.com>
2025-01-08 14:16:48 -0500

    [Step Function] 3.3. Add class StepFunctionConfigLoader (#151)

---
+++ serverless/src/step_function/env.ts
@@ -0,0 +1,24 @@
+import { ConfigLoader } from "../env";
+
+export interface Configuration {
+  // When set, it will be added to the state machine's log group name.
+  env?: string;
+}
+
+const envEnvVar = "DD_ENV";
+
+export class StepFunctionConfigLoader extends ConfigLoader<Configuration> {
+  readonly defaultConfiguration: Configuration = {};
+
+  public getConfigFromEnvVars(): Configuration {
+    const config: Configuration = {
+      ...this.defaultConfiguration,
+    };
+
+    if (envEnvVar in process.env) {
+      config.env = process.env[envEnvVar];
+    }
+
+    return config;
+  }
+}
