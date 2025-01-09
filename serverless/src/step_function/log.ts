428f94ed00bcdd2a26c5d047953ad3d0a274a445
Yiming Luo <yiming.luo@datadoghq.com>
2025-01-09 13:58:34 -0500

    [Step Function] 4. Add permissions to state machine role (#153)

--- serverless/src/step_function/log.ts
+++ serverless/src/step_function/log.ts
@@ -3,11 +3,32 @@ import log from "loglevel";
 import { StateMachine } from "./types";
 import { Configuration } from "./env";
 
+const unsupportedCaseErrorMessage =
+  "Step Function Instrumentation is not supported. \
+Please open a feature request in https://github.com/DataDog/datadog-cloudformation-macro.";
+
+const FN_SUB = "Fn::Sub";
+export const FN_GET_ATT = "Fn::GetAtt";
+// Permissions required for the state machine role to log to CloudWatch Logs
+export const ROLE_ACTIONS = [
+  "logs:CreateLogDelivery",
+  "logs:CreateLogStream",
+  "logs:GetLogDelivery",
+  "logs:UpdateLogDelivery",
+  "logs:DeleteLogDelivery",
+  "logs:ListLogDeliveries",
+  "logs:PutLogEvents",
+  "logs:PutResourcePolicy",
+  "logs:DescribeResourcePolicies",
+  "logs:DescribeLogGroups",
+];
+
 /**
  * Set up logging for the given state machine:
  * 1. Set log level to ALL
  * 2. Set includeExecutionData to true
  * 3. Create a destination log group (if not set already)
+ * 4. Add permissions to the state machine role to log to CloudWatch Logs
  */
 export function setUpLogging(resources: Resources, config: Configuration, stateMachine: StateMachine): void {
   log.debug(`Setting up logging`);
@@ -46,6 +67,86 @@ function createLogGroup(resources: Resources, config: Configuration, stateMachin
       RetentionInDays: 7,
     },
   };
+
+  let role;
+  if (stateMachine.properties.RoleArn) {
+    log.debug(`A role is already defined. Parsing its resource key from the roleArn.`);
+    const roleArn = stateMachine.properties.RoleArn;
+
+    // We assume that if a role is set on the state machine, then it is defined in the same
+    // CloudFormation stack (and roleArn is likely an object that references the role),
+    // so we can add a permission policy to the role. Otherwise, if the roleArn is
+    // a hard-coded string, which likely means the role is defined outside the stack, then
+    // we will need to explore other ways to add the permission policy.
+    if (typeof roleArn !== "object") {
+      throw new Error(`RoleArn is not an object: ${roleArn}. ${unsupportedCaseErrorMessage}`);
+    }
+
+    // There are many ways a user can specify a state machine role in a CloudFormation template. For now
+    // we only support the simple cases. We can support more cases as needed.
+    // For each case, extract the role key from roleArn.
+    let roleKey;
+    if (roleArn[FN_GET_ATT]) {
+      // e.g.
+      //   Fn::GetAtt: [MyStateMachineRole, "Arn"]
+      roleKey = roleArn[FN_GET_ATT][0];
+    } else if (roleArn[FN_SUB]) {
+      // e.g.
+      //   Fn::Sub: ${StatesExecutionRole.Arn}
+      const arnMatch = roleArn[FN_SUB].match(/^\${(.*)\.Arn}$/);
+      if (arnMatch) {
+        roleKey = arnMatch[1];
+      } else {
+        throw new Error(`Unsupported Fn::Sub format: ${roleArn[FN_SUB]}. ${unsupportedCaseErrorMessage}`);
+      }
+    } else {
+      throw new Error(`Unsupported RoleArn format: ${roleArn}. ${unsupportedCaseErrorMessage}`);
+    }
+    log.debug(`Found State Machine role Key: ${roleKey}`);
+    role = resources[roleKey];
+  } else {
+    log.debug(`No role is defined. Creating one.`);
+    const roleKey = `${stateMachine.resourceKey}Role`;
+    role = {
+      Type: "AWS::IAM::Role",
+      Properties: {
+        AssumeRolePolicyDocument: {
+          Version: "2012-10-17",
+          Statement: [
+            {
+              Effect: "Allow",
+              Principal: {
+                Service: "states.amazonaws.com",
+              },
+              Action: "sts:AssumeRole",
+            },
+          ],
+        },
+        Policies: [],
+      },
+    };
+    resources[roleKey] = role;
+    stateMachine.properties.RoleArn = { FN_GET_ATT: [roleKey, "Arn"] };
+  }
+
+  log.debug(`Adding a policy to the role to grant permissions to the log group`);
+  if (!role.Properties.Policies) {
+    role.Properties.Policies = [];
+  }
+  role.Properties.Policies.push({
+    PolicyName: `${stateMachine.resourceKey}LogPolicy`,
+    // Copied from https://docs.aws.amazon.com/step-functions/latest/dg/cw-logs.html#cloudwatch-iam-policy
+    PolicyDocument: {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Effect: "Allow",
+          Action: ROLE_ACTIONS,
+          Resource: "*",
+        },
+      ],
+    },
+  });
 
   return logGroupKey;
 }
