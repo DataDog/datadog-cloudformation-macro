6248bb120e5bee3aaab21e27c0009374740393e7
Yiming Luo <yiming.luo@datadoghq.com>
2025-01-14 17:21:38 -0500

    [Step Function] 6.3 Set tags (#157)

--- serverless/src/step_function/env.ts
+++ serverless/src/step_function/env.ts
@@ -1,8 +1,15 @@
 import { ConfigLoader } from "../common/env";
+import { ConfigurationWithTags } from "common/tags";
 
-export interface Configuration {
-  // When set, it will be added to the state machine's log group name.
+export interface Configuration extends ConfigurationWithTags {
+  // When set, it will be added to the state machine's log group name and added as a tag.
   env?: string;
+  // When set, it will be added as a tag of the state machine.
+  service?: string;
+  // When set, it will be added as a tag of the state machine.
+  version?: string;
+  // Custom tags to be added to the state machine, in the format "key1:value1,key2:value2".
+  tags?: string;
   // When set, the forwarder will subscribe to the state machine's log group.
   stepFunctionForwarderArn?: string;
 }
