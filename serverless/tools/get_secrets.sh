ce533829a6ba16af0716e154763e1b8a09ef5865
Hannah Jiang <hannah.jiang@datadoghq.com>
2025-02-24 12:53:49 -0500

    use script from other repos

--- serverless/tools/get_secrets.sh
+++ serverless/tools/get_secrets.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 
-# Retrieve External ID from Vault
-echo "Retrieving External ID..."
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the Apache License Version 2.0.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2025 Datadog, Inc.
+
+set -e
+
+if [ -z "$EXTERNAL_ID_NAME" ]; then
+    printf "[Error] No EXTERNAL_ID_NAME found.\n"
+    printf "Exiting script...\n"
+    exit 1
+fi
+
+if [ -z "$ROLE_TO_ASSUME" ]; then
+    printf "[Error] No ROLE_TO_ASSUME found.\n"
+    printf "Exiting script...\n"
+    exit 1
+fi
+
+printf "Getting AWS External ID...\n"
 EXTERNAL_ID=$(vault kv get -field="${EXTERNAL_ID_NAME}" kv/k8s/gitlab-runner/datadog-cloudformation-macro/secrets)
 
-# Ensure EXTERNAL_ID is retrieved
-if [ -z "$EXTERNAL_ID" ]; then
-    echo "[Error] Failed to retrieve EXTERNAL_ID."
-    exit 1
-fi
-
-# Assume AWS IAM Role
-echo "Assuming AWS IAM Role..."
-CREDENTIALS=$(aws sts assume-role \
-    --role-arn "arn:aws:iam::${ACCOUNT}:role/${ROLE_TO_ASSUME}" \
-    --role-session-name "ci.datadog-cloudformation-macro-${CI_JOB_ID}-${CI_JOB_STAGE}" \
+printf "Assuming role...\n"
+export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
+    $(aws sts assume-role \
+    --role-arn "arn:aws:iam::$AWS_ACCOUNT:role/$ROLE_TO_ASSUME"  \
+    --role-session-name "ci.serverless-e2e-tests-$CI_JOB_ID-$CI_JOB_STAGE" \
     --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
-    --external-id "${EXTERNAL_ID}" \
-    --output text)
-
-# Ensure credentials were retrieved
-if [ -z "$CREDENTIALS" ]; then
-    echo "[Error] Failed to assume AWS IAM role."
-    exit 1
-fi
-
-# Parse AWS credentials
-read AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN <<< "$CREDENTIALS"
-
-# Export AWS credentials for future commands
-export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
-
-# Success message
-echo "AWS credentials successfully retrieved and exported!"
+    --external-id $EXTERNAL_ID \
+    --output text))
