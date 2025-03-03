cc17ba2b125a21b0dad63a5d2ac7cccb77dac57c
Hannah Jiang <hannah.jiang@datadoghq.com>
2025-03-03 13:31:14 -0500

    fix more paths

--- serverless/release.sh
+++ serverless/release.sh
@@ -12,8 +12,10 @@ else
     BUCKET=$1
 fi
 
+cd serverless
+
 # Read the current version
-CURRENT_VERSION=$(grep -o 'Version: \d\+\.\d\+\.\d\+' ./serverless/template.yml | cut -d' ' -f2)
+CURRENT_VERSION=$(grep -o 'Version: \d\+\.\d\+\.\d\+' template.yml | cut -d' ' -f2)
 
 # Do a production release (default is staging) - useful for developers
 if [[ $# -eq 3 ]] && [[ $3 = "--prod" ]]; then
@@ -27,13 +29,12 @@ aws sts get-caller-identity
 
 # Validate the template
 echo "Validating template.yml"
-aws cloudformation validate-template --template-body file://./serverless/template.yml
+aws cloudformation validate-template --template-body file://template.yml
 
 # Build and run test suite
 echo "Running unit tests and build script"
 
 yarn add --dev @types/jest
-cd serverless
 yarn test
 
 echo "$CI_PIPELINE_SOURCE"
@@ -49,7 +50,7 @@ if [ "$PROD_RELEASE" = true ] ; then
 
     VERSION=$(echo "${CI_COMMIT_TAG##*v}" | cut -d'-' -f3-)
 
-    if [[ ! $(./serverless/tools/semver.sh "$VERSION" "$CURRENT_VERSION") > 0 ]]; then
+    if [[ ! $(tools/semver.sh "$VERSION" "$CURRENT_VERSION") > 0 ]]; then
         echo "Must use a version greater than the current ($CURRENT_VERSION)"
         exit 1
     fi
@@ -59,19 +60,19 @@ if [ "$PROD_RELEASE" = true ] ; then
 
     # Bump version number
     echo "Bumping the current version number to the desired"
-    perl -pi -e "s/Version: ${CURRENT_VERSION}/Version: ${VERSION}/g" ./serverless/template.yml
+    perl -pi -e "s/Version: ${CURRENT_VERSION}/Version: ${VERSION}/g" template.yml
 
     yarn version --no-git-tag-version --new-version "${VERSION}"
     
 
     # Commit version number changes to git
-    git add src/ ./serverless/template.yml README.md package.json
+    git add src/ template.yml README.md package.json
     git commit -m "Bump version from ${CURRENT_VERSION} to ${VERSION}"
     git push origin main
 
     # Create a github release
     echo "Release serverless-macro-${VERSION} to github"
-    ./serverless/tools/build_zip.sh "${VERSION}"
+    tools/build_zip.sh "${VERSION}"
 
     gh release create serverless-macro-${VERSION} .macro/serverless-macro-${VERSION}.zip --generate-notes
     TEMPLATE_URL="https://${BUCKET}.s3.amazonaws.com/aws/serverless-macro/latest.yml"
@@ -87,21 +88,20 @@ else
 fi
 
 # Upload the template to the S3 bucket
-# echo "Uploading template.yml to s3://${BUCKET}/aws/serverless-macro/${VERSION}.yml"
-
 if [ "$PROD_RELEASE" = true ] ; then
-    aws s3 cp ./serverless/template.yml s3://${BUCKET}/aws/serverless-macro/${VERSION}.yml \
+    echo "Uploading template.yml to s3://${BUCKET}/aws/serverless-macro/${VERSION}.yml"
+    aws s3 cp template.yml s3://${BUCKET}/aws/serverless-macro/${VERSION}.yml \
         --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-    aws s3 cp ./serverless/template.yml s3://${BUCKET}/aws/serverless-macro/latest.yml \
+    aws s3 cp template.yml s3://${BUCKET}/aws/serverless-macro/latest.yml \
         --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
     echo "Version ${VERSION} has been released"
     echo "Update release notes with included PRs: https://github.com/DataDog/datadog-cloudformation-macro/releases/tag/serverless-macro-${VERSION}"
-# else
-    # aws s3 cp ./serverless/template.yml s3://${BUCKET}/aws/serverless-macro-staging/${VERSION}.yml
-    # aws s3 cp ./serverless/template.yml s3://${BUCKET}/aws/serverless-macro-staging/latest.yml
-    # echo "Dev version ${VERSION} has been released"
-    
-# fi
+else
+    aws s3 cp template.yml s3://${BUCKET}/aws/serverless-macro-staging/${VERSION}.yml
+    aws s3 cp template.yml s3://${BUCKET}/aws/serverless-macro-staging/latest.yml
+    echo "Dev version ${VERSION} has been released"
+
+fi
 
 # echo "Done uploading the template, and here is the CloudFormation quick launch URL"
 # echo "https://console.aws.amazon.com/cloudformation/home#/stacks/quickCreate?stackName=datadog-serverless-macro&templateURL=${TEMPLATE_URL}"
