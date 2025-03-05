35129427b09dccfc2d12baaf4fb039fb06036ca8
Hannah Jiang <hannah.jiang@datadoghq.com>
2025-03-05 16:49:03 -0500

    uncomment the login

--- serverless/release.sh
+++ serverless/release.sh
@@ -60,14 +60,13 @@ if [ "$PROD_RELEASE" = true ] ; then
     alias gh="env -u GITHUB_TOKEN gh $1"
     gh auth status 
     # if [[ $(command -v gh) ]]; then
-        # The workaround needs user scope and at least for corporate accounts, that isn't the default
-        # gh auth refresh -h github.com -s user
+        gh auth refresh -h github.com -s user
 
-        # user=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user | jq -r .login)
-        # email=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user/emails | jq -r ".[0].email")
-        # echo "Setting $user <$email> as the default Git user..."
-        # git config --global user.name "$user"
-        # git config --global user.email "$email"
+        user=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user | jq -r .login)
+        email=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user/emails | jq -r ".[0].email")
+        echo "Setting $user <$email> as the default Git user..."
+        git config --global user.name "$user"
+        git config --global user.email "$email"
     # fi
 
     # Get the latest code
