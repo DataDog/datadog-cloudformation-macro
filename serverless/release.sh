e1f33845e437e55d7e39c14bad6db4cd0eb8943b
Hannah Jiang <hannah.jiang@datadoghq.com>
2025-03-05 16:38:58 -0500

    comment out

--- serverless/release.sh
+++ serverless/release.sh
@@ -60,16 +60,16 @@ if [ "$PROD_RELEASE" = true ] ; then
     # alias gh="env -u GITHUB_TOKEN gh $1"
     gh auth status 
   
-    if [[ $(command -v gh) ]]; then
+    # if [[ $(command -v gh) ]]; then
         # The workaround needs user scope and at least for corporate accounts, that isn't the default
-        gh auth refresh -h github.com -s user
+        # gh auth refresh -h github.com -s user
 
         user=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user | jq -r .login)
         email=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user/emails | jq -r ".[0].email")
         echo "Setting $user <$email> as the default Git user..."
         git config --global user.name "$user"
         git config --global user.email "$email"
-    fi
+    # fi
 
     # Get the latest code
     # git pull origin main 
