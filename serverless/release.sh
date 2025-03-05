dda51e94545c3c739524fde75f5909ee9c39cfa3
Hannah Jiang <hannah.jiang@datadoghq.com>
2025-03-05 16:47:06 -0500

    use alias

--- serverless/release.sh
+++ serverless/release.sh
@@ -57,17 +57,17 @@ if [ "$PROD_RELEASE" = true ] ; then
 
     echo "Setting origin to github.com"
     git remote set-url origin https://github.com/DataDog/datadog-cloudformation-macro.git
-    # alias gh="env -u GITHUB_TOKEN gh $1"
+    alias gh="env -u GITHUB_TOKEN gh $1"
     gh auth status 
     # if [[ $(command -v gh) ]]; then
         # The workaround needs user scope and at least for corporate accounts, that isn't the default
-        gh auth refresh -h github.com -s user
+        # gh auth refresh -h github.com -s user
 
-        user=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user | jq -r .login)
-        email=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user/emails | jq -r ".[0].email")
-        echo "Setting $user <$email> as the default Git user..."
-        git config --global user.name "$user"
-        git config --global user.email "$email"
+        # user=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user | jq -r .login)
+        # email=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user/emails | jq -r ".[0].email")
+        # echo "Setting $user <$email> as the default Git user..."
+        # git config --global user.name "$user"
+        # git config --global user.email "$email"
     # fi
 
     # Get the latest code
