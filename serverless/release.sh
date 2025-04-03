89a15d46bbc71ea2de7f1a6aaba70fa641ac0952
TophrC-dd <topher.campbell@datadoghq.com>
2025-04-03 10:40:53 -0400

    just passing values through the url params is a less complicated approach

--- serverless/release.sh
+++ serverless/release.sh
@@ -109,21 +109,13 @@ if [ "$PROD_RELEASE" = true ] ; then
     MACRO_SOURCE_URL="https://github.com/DataDog/datadog-cloudformation-macro/releases/download/serverless-macro-${VERSION}/serverless-macro-${VERSION}.zip'"
 else
     VERSION=$CI_COMMIT_SHA
-    MACRO_SOURCE_URL="s3://${BUCKET}/aws/serverless-macro-staging-zip/serverless-macro-${VERSION}.zip"
+    MACRO_SOURCE_URL="https://${BUCKET}.s3.amazonaws.com/aws/serverless-macro-staging-zip/serverless-macro-${VERSION}.zip"
     TEMPLATE_URL="https://${BUCKET}.s3.amazonaws.com/aws/serverless-macro-staging/latest.yml"
-    
-    #Replace the current version with the SHA hash of the commit
-    echo "Setting current version to ${VERSION}"
-    perl -pi -e "s/Version: ${CURRENT_VERSION}/Version: ${VERSION}/g" template.yml
-    
-    # Replace the default SourceZipUrl to the staging path
-    echo "Replacing the default SourceZipUrl to the staging path"
-    perl -pi -e "s/github.com\/DataDog\/datadog-cloudformation-macro\/releases\/download\/serverless-macro-\${VERSION}/${BUCKET}.s3.amazonaws.com\/aws\/serverless-macro-staging-zip\//g" template.yml
-    
+ 
     # Upload to s3 instead of github
     echo "About to release non-public staging version of macro, upload serverless-macro-${VERSION} to s3, and upload the template.yml to s3://${BUCKET}/aws/serverless-macro-staging/${VERSION}.yml"
     tools/build_zip.sh "${VERSION}"
-    aws s3 cp .macro/serverless-macro-${VERSION}.zip ${MACRO_SOURCE_URL}
+    aws s3 cp .macro/serverless-macro-${VERSION}.zip s3://${BUCKET}/aws/serverless-macro-staging-zip/serverless-macro-${VERSION}.zip
 fi
 
 # Upload the template to the S3 bucket
@@ -143,7 +135,7 @@ else
 fi
 
 echo "Done uploading the template, and here is the CloudFormation quick launch URL"
-echo "https://console.aws.amazon.com/cloudformation/home#/stacks/quickCreate?stackName=datadog-serverless-macro&templateURL=${TEMPLATE_URL}"
+echo "https://console.aws.amazon.com/cloudformation/home#/stacks/quickCreate?stackName=datadog-serverless-macro&templateURL=${TEMPLATE_URL}&param_SourceZipUrl=${MACRO_SOURCE_URL}"
 
 echo "Done!"
 
